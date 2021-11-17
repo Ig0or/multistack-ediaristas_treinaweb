50e18794f16831bf75169dd986f8ef27e42edf50
Ig0or <igorprovensi@gmail.com>
2021-11-17 13:59:17 -0300

    ativando pagina de login

--- ediaristas/administracao/urls.py
+++ ediaristas/administracao/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import servico_views, usuario_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Serviços
@@ -9,5 +10,7 @@ urlpatterns = [
     # Usuários
     path('usuarios/cadastrar', usuario_views.cadastrar_usuario, name='cadastrar_usuario'),
     path('usuarios/listar', usuario_views.listar_usuarios, name='listar_usuarios'),
-    path('usuarios/editar/<int:id>', usuario_views.editar_usuario, name='editar_usuario')
+    path('usuarios/editar/<int:id>', usuario_views.editar_usuario, name='editar_usuario'),
+    # Autenticação
+    path('autenticacao/login', auth_views.LoginView.as_view(), name='logar_usuario'),
 ]
